8b673d765a0fd43648101ad44ace850929b46ebe
PieStw <piero.stalteri@icloud.com>
2024-10-18 15:26:18 +0200

    esercizio palindromi

--- js-pali/script.js
+++ js-pali/script.js
@@ -2,4 +2,16 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
 
+function checkPalindroma (parola) {
+    let check = true;
+    for(let i = 0; i < parola.length/2; i++){
+        if(parola[i] !== parola[parola.length - 1 - i]){
+            check = false;
+            return check;
+        }
+    }
+    return check;
+}
 
+const parola = prompt("Inserisci una parola");
+console.log(`${parola} è palindroma: ` + (checkPalindroma(parola) ? "Si" : "No"));
