ba4d1a20ed0d7d512f375cb3129ecc41ca09a11a
PieStw <piero.stalteri@icloud.com>
2024-10-18 15:45:53 +0200

    esercizio pari o dispari

--- js-dispari/script.js
+++ js-dispari/script.js
@@ -5,4 +5,21 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto. */
 
+function checkPariODispari (numero){
+    return numero % 2 == 0
+}
 
+let pari = false;
+const scelta = prompt("pari o dispari?");
+pari = (scelta == "pari" ? true : false);
+
+
+const numPlayer = parseInt(prompt("Inserisci un numero"));
+const numComputer = Math.floor(Math.random() * 6 + 1);
+console.log(numPlayer);
+console.log(numComputer);
+
+
+const somma = numPlayer + numComputer;
+console.log("La somma tra il tuo numero e quello del pc è: " + (checkPariODispari(somma) ? "Pari " : "Dispari "))
+console.log(checkPariODispari(somma) == pari ? "Hai vinto " : "Hai perso ");
